d4d2cd0c8dc1b87d89c1b9e5e152836cbab5bb2d
ymzkryo <ymzk.yamachan@gmail.com>
2021-11-01 14:16:15 +0900

    update: change path

--- zsh/411_todoist.zsh
+++ zsh/411_todoist.zsh
@@ -2,4 +2,4 @@
 # sachaos/todoist function
 ###################################
 
-source "$GOPATH/pkg/mod/github.com/sachaos/todoist@v0.15.0/todoist_functions.sh"
+source "$GOPATH/src/github.com/sachaos/todoist/todoist_functions.sh"
