07bc98eea661a71fdc6f0c2847c84e80cfa347a2
ymzkryo <ymzk.yamachan@gmail.com>
2024-01-18 10:07:44 +0900

    Update

--- zsh/415_asdf.zsh
+++ zsh/415_asdf.zsh
@@ -1,4 +1,9 @@
 fpath=(${ASDF_DIR}/completions $fpath)
 autoload -Uz compinit && compinit
 
+# golang
 . ~/.asdf/plugins/golang/set-env.zsh
+
+# rust
+export RUST_WITHOUT=rust-docs
+source $HOME/.asdf/installs/rust/1.75.0/env
