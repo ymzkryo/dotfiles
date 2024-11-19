48239e0484337c1b5ead63b126439358d3f677f9
ymzkryo <ymzk.yamachan@gmail.com>
2024-11-19 18:11:52 +0900

    🧹Remove: zsh/

--- zsh/411_todoist.zsh
+++
@@ -1,5 +0,0 @@
-###################################
-# sachaos/todoist function
-###################################
-
-source ~/.asdf/installs/golang/1.21.6/packages/pkg/mod/github.com/sachaos/todoist/todoist_functions.sh
